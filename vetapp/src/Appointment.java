e545d943826d853bcbcae34e6c48af3601407bf3
Furkan Yakkan <80443639+fyakkan@users.noreply.github.com>
2022-04-22 06:34:42 +0300

    Delete vetapp directory

--- vetapp/src/Appointment.java
+++
@@ -1,7 +0,0 @@
-public class Appointment {
-    String date;
-    User user;
-    Vet veteriner;
-    Pet pet;
-
-}
--- vetapp/src/Clinic.java
+++
@@ -1,7 +0,0 @@
-public class Clinic {
-    String clinicName;
-    int clinicID;
-    int clinicTelnumber;
-    String clinicAdress;
-
-}
